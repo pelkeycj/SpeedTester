421538a1c14626b0ced9fed26d0c1bf000497599
Connor Pelkey <Pelkey.c@husky.neu.edu>
2016-11-02 15:01:02 -0400

    minor organization

---
+++ main.py
@@ -0,0 +1,15 @@
+'''
+Test internet speed every hour, log results
+'''
+
+from SpeedTest import *
+
+def main():
+    test = SpeedTest()
+    test.runTest()
+
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ speedtest.py
@@ -0,0 +1,16 @@
+import os, datetime
+
+class SpeedTest():
+    def __init__(self):
+        self.ping = 0
+        self.down = 0
+        self.up = 0
+        self.timestamp = datetime.datetime.now()
+
+    #run speedtest
+    def runTest(self):
+        self.results = os.popen("/usr/local/bin/speedtest-cli --simple").read().split()
+        if 'Cannot' not in self.results:
+            self.ping = float(self.results[1])
+            self.down = float(self.results[4])
+            self.up = float(self.results[7])
