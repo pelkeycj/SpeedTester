9e97f3e04635276b95f4cd658a3c3bad85c04c5a
Connor Pelkey <Pelkey.c@husky.neu.edu>
2016-11-12 01:53:02 -0500

    adds ip address validation

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 import os
+import requests
+import pickle
 import matplotlib.dates
 from matplotlib import pyplot as plt
 from matplotlib import style
@@ -12,12 +14,12 @@ def main():
     path = os.path.abspath(__file__)
     makeCron(path)
 
-    test = SpeedTest()
-    test.runTest()
-    test.logData(path)
+    if checkIP():
+        test = SpeedTest()
+        test.runTest()
+        test.logData(path)
 
-
-''' Creates a cron job to schedule program if needed '''
+''' Creates a Cron job for the program if needed '''
 def makeCron(path):
     speedtest_ID = "id: SpeedTester"
     cron = CronTab(user=True)
@@ -32,15 +34,40 @@ def makeCron(path):
     dir_path = path[:-7]
     job = cron.new(command= "python %s\n" % path)
     job.set_comment(speedtest_ID)
-    job.setall('* * * * *')
+    job.setall('* 0 * * *')
     cron.write()
 
+''' Gets the IP address for speed monitoring
+    Sets the address if not exists'''
+def getIP():
+    # open and read stored address data
+    try:
+        addr_file = open('address.obj', 'r')
+        address = pickle.load(addr_file)
+    # else create new address file
+    except IOError:
+        address = requests.request('GET', 'http://myip.dnsomatic.com').text
+        addr_file = open('address.obj', 'w')
+        pickle.dump(address, addr_file)
+
+    return address
+
+''' Checks the users IP address to determine
+    if the speed test should be run '''
+def checkIP():
+        current_ip = requests.request('GET', 'http://myip.dnsomatic.com').text
+        test_ip = getIP()
+
+        if current_ip == test_ip:
+            return True
+        else:
+            return False
 
 
 ''' Clear all data from database '''
 def clearData(path):
     dir_path = path[:-7]
-    conn = lite.connect(path + 'test.db')
+    conn = lite.connect(dir_path + 'test.db')
 
     with conn:
         cursor = conn.cursor()
@@ -49,7 +76,7 @@ def clearData(path):
 ''' Plot data '''
 def plotData(path):
     dir_path = path[:-7]
-    conn = lite.connect(path + 'test.db')
+    conn = lite.connect(dir_path + 'test.db')
     with conn:
         cursor = conn.cursor()
         cursor.execute("SELECT * FROM Tests")
